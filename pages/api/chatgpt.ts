88060c1420db3a5e4e120431ab22e2c9c3586ccb
daviddeek <109833309+daviddeek@users.noreply.github.com>
2023-11-11 19:20:15 -0600

    Update chatgpt.ts

--- pages/api/chatgpt.ts
+++ pages/api/chatgpt.ts
@@ -28,7 +28,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         try {
             const response: any = await openai.createChatCompletion({
-                model: "gpt-4-1106-preview",
+                model: "gpt-4",
                 // @ts-ignore
                 messages: messages,
                 temperature: .7,
